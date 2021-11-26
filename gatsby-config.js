d3c4f6017e04ca456e3fc57c102f70b3667d0433
marxxbluecode <marxx171@gmail.com>
2021-11-26 19:31:13 -0300

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My personnal Blog",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: ["gatsby-plugin-styled-components",
+            "gatsby-plugin-react-helmet",
+          {
+            resolve: "gatsby-source-graphql",
+            options: {
+              typeName: "datablog",
+              fieldName: "datablog",
+              url: "https://api.graph.cool/simple/v1/cj7q0q0q0q0q0101x0x0x0x0",
+          }
+        },
+      ],
 };
